d7585dede79b1db04c9fd387113b7a32bffbb858
DevJhoan <znotjhoan@gmail.com>
2024-11-09 21:35:28 -0500

    feat: send message to all players when a player revive

--- src/main/java/cc/atenea/dedsafioUtils/events/PlayerJoinEvent.java
+++ src/main/java/cc/atenea/dedsafioUtils/events/PlayerJoinEvent.java
@@ -1,9 +1,11 @@
 package cc.atenea.dedsafioUtils.events;
 
 import cc.atenea.dedsafioUtils.DedsafioPlugin;
+import cc.atenea.dedsafioUtils.providers.User;
 import cc.atenea.dedsafioUtils.resources.types.ConfigResource;
 import cc.atenea.dedsafioUtils.utilities.ChatUtil;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,9 +14,23 @@ public class PlayerJoinEvent implements Listener {
   @EventHandler
   public void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent event) {
     Player player = event.getPlayer();
+    User userProfile = DedsafioPlugin.getInstance().userManager.getUser(player);
     event.setJoinMessage(null);
 
+    if (userProfile.isDead()) {
+      player.kickPlayer(ChatUtil.translate("&aEstás muerto, alguien debe revivirte."));
+      return;
+    }
+
+    if (userProfile.getAlertRevive()) {
+      ChatUtil.broadcast("&3✧ &b&l" + player.getName() + " &3&lha sido resucitado! ✧");
+      userProfile.setAlertRevive(false);
+    }
+
     if (player.getGameMode() == GameMode.SPECTATOR) {
+      Location spawnLocation = player.getWorld().getSpawnLocation();
+
+      player.teleport(spawnLocation);
       player.setGameMode(GameMode.SURVIVAL);
     }
 
@@ -26,6 +42,5 @@ public class PlayerJoinEvent implements Listener {
       );
     }
 
-    DedsafioPlugin.getInstance().userManager.getUser(player.getUniqueId());
   }
 }
